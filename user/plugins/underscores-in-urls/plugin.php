e9d87d1749e1dfd60fa002fe27e4985e49bc1d50
Mathew Gancarz <mathew.gancarz@desouzainstitute.com>
2014-01-31 16:27:45 -0500

    code fixes

--- user/plugins/underscores-in-urls/plugin.php
+++ user/plugins/underscores-in-urls/plugin.php
@@ -11,9 +11,8 @@ Author URI: http://desouzainstitute.com
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-yourls_add_filter( 'get_shorturl_charset', 'ozh_hyphen_in_charset' );
-function ozh_hyphen_in_charset( $in ) {
+yourls_add_filter( 'get_shorturl_charset', 'dsi_underscore_in_charset' );
+function dsi_underscore_in_charset( $in ) {
 	return $in.'_';
 }
-
 
