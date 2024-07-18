7e4513cd369d4a362bb0259ddbd963d08da4a878
Mathew Gancarz <mathew.gancarz@desouzainstitute.com>
2024-07-17 22:54:40 -0400

    added underscore-in-urls plugins we built

---
+++ user/plugins/underscores-in-urls/README.md
@@ -0,0 +1,5 @@
+Underscores in URLs
+===============
+This is a copy of the Hyphens in URLs plugin, bundled with YOURLS. This one allows underscore characters, such as _ .
+
+Last Updated by: Mgancarz - 2014/01/31
---
+++ user/plugins/underscores-in-urls/plugin.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Allow Underscores in Short URLs
+Plugin URI: http://desouzainstitute.com
+Description: Allow Underscores in short URLs (like <tt>http://sho.rt/hello_world</tt>)
+Version: 1.0
+Author: Mathew Gancarz
+Author URI: http://desouzainstitute.com
+*/
+
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+yourls_add_filter( 'get_shorturl_charset', 'dsi_underscore_in_charset' );
+function dsi_underscore_in_charset( $in ) {
+	return $in.'_';
+}
+
